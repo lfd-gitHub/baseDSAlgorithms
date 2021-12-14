928ef6e9118943c0dc4c528f77eb375f245afc1a
lfd <lfd_wy@163.com>
2021-12-14 09:19:09 +0800

    init

--- src/com/lfd/jdsaa/App.java
+++
@@ -1,9 +0,0 @@
-package com.lfd.jdsaa;
-
-
-
-public class App {
-    public static void main(String[] args) throws Exception {
-
-    }
-}
--- src/com/lfd/jdsaa/erecursion/hanoiui/IUiPart.java
+++
@@ -1,9 +0,0 @@
-package com.lfd.jdsaa.erecursion.hanoiui;
-import java.awt.*;
-
-public interface IUiPart {
-    public void draw(Graphics2D g);
-    public Rect getBounds();
-    public IUiPart setBounds(int x,int y,int w,int h);
-    public void offset(int x,int y);
-}
